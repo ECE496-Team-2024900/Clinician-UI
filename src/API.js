d16aecd0694055a65540c33a165ccb320a0e3e7c
faatima_abidi <faatima.abidi24@gmail.com>
2024-11-05 13:02:41 -0500

    Added the base code for a video call

---
+++ src/API.js
@@ -0,0 +1,16 @@
+//This is the Auth token, you will use it to generate a meeting and connect to it
+export const authToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlrZXkiOiI5YTcwZWFiNS1lZGUzLTQ2NGUtODllYS1mNmY2MjNhNjA0YjYiLCJwZXJtaXNzaW9ucyI6WyJhbGxvd19qb2luIl0sImlhdCI6MTczMDc3MzM3MywiZXhwIjoxNzMxMzc4MTczfQ.yoG1fUcUoWpOwYW2qwAObXced8_Bb6qI4ddXSlYI_NM";
+// API call to create a meeting
+export const createMeeting = async ({ token }) => {
+    const res = await fetch(`https://api.videosdk.live/v2/rooms`, {
+        method: "POST",
+        headers: {
+            authorization: `${authToken}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({}),
+    });
+    //Destructuring the roomId from the response
+    const { roomId } = await res.json();
+    return roomId;
+};
