98509d4b2f4a4083f1764175e235a82de0a666e1
niloferh <nilofer.hyder@mail.utoronto.ca>
2024-11-25 15:41:17 -0500

    Switched from using Title to HTML headings

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,7 @@
-import { Button, Input, Typography } from 'antd';
-import logo from "../assets/logo.png"
+import { Button, Input } from 'antd';
+import logo from "../assets/logo.png";
 import styles from "../css/Login.module.css";
 import { useNavigate } from "react-router-dom";
-
-const { Title } = Typography;
 
 function Login() {
     const navigate = useNavigate(); 
@@ -12,16 +10,16 @@ function Login() {
         //navigate("/home"); // NOTE: Update once the next page is implemented
     };
 
-    return(
+    return (
         <div className={styles.page}>
-            <img src={logo} alt={"logo"} width={"50%"} height={"100%"}/>
+            <img src={logo} alt={"logo"} width={"50%"} height={"100%"} />
             <div className={styles.form}>
-                <Title class={styles.title}>
+                <h1 className={styles.title}>
                     Welcome back
-                </Title>
-                <Title class={styles.subtitle} level={2}>
+                </h1>
+                <h2 className={styles.subtitle}>
                     Please enter your email
-                </Title>
+                </h2>
                 <Input 
                     placeholder="Your email" 
                     className={styles.email_input}
