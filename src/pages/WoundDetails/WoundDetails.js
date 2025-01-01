c8621d93ef6fe13ad9a12e5676c20369158716e8
zoya5636 <zoya@chishtie.com>
2025-01-01 16:26:35 -0500

    added comments

--- src/pages/WoundDetails/WoundDetails.js
+++ src/pages/WoundDetails/WoundDetails.js
@@ -5,17 +5,19 @@ import { message } from 'antd';
 import styles from "../../css/WoundDetails.module.css";
 
 function WoundDetails() {
-    const [pastTreatments, setPastTreatments] = useState([]);
+    const [pastTreatments, setPastTreatments] = useState([]); // keeping track of past treatments for this wound and patient
 
     // Temporary variables - replace once logic implemented for it
     const woundId = 1
     const patientId = 1
 
     useEffect(() => {
+        // fetching past treatments
         const fetchPastTreatments = async () => {
             const url = `${getTreatmentAPIUrl()}/treatment/get_past_patient_treatments?patient_id=${patientId}&wound_id=${woundId}`;
             axios.get(url)
             .then((response) => {
+                // if there are no errors and past treatmentts are available, storing them in use state
                 if(response.status === 200) {
                     setPastTreatments(response.data);
                 }
@@ -27,6 +29,7 @@ function WoundDetails() {
         fetchPastTreatments()
     }, [])
 
+    // helper function to format date from 'YYYY-mm-dd' to 'weekday, month date, year' (e.g. Tuesday, November 26, 2024)
     const formatDate = (dateString) => {
         const date = new Date(dateString);
         const options = { 
