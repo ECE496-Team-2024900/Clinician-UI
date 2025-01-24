b923976aa2546a171144740335b069b312c0b196
zoya5636 <zoya@chishtie.com>
2025-01-24 16:58:03 -0500

    Added fields and CSV option

--- src/pages/PostTreatment/PostTreatment.js
+++ src/pages/PostTreatment/PostTreatment.js
@@ -5,13 +5,20 @@ import { message, Button } from 'antd';
 import axios from 'axios';
 import ReportGeneration from '../../utilities/ReportGeneration/ReportGeneration';
 import styles from '../../css/PostTreatment.module.css'
+import { SaveOutlined } from '@ant-design/icons';
 
 function PostTreatment() {
-    const patientEmail = "mickey.mouse@disney.org"; // Assuming this is provided by the previous page
-    const treatmentId = 1; // Assuming this is provided by the previous page
+
+    // Assuming that the following 4 fields are provided by the previous page
+    const patientEmail = "mickey.mouse@disney.org";
+    const treatmentId = 1;
+    const clinicianEmail = "walt.disney@disney.org";
+    const woundId = 1;
 
     const [patientData, setPatientData] = useState(null);
     const [treatmentData, setTreatmentData] = useState(null);
+    const [clinicianData, setClinicianData] = useState(null);
+    const [woundData, setWoundData] = useState(null);
     const [fileData, setFileData] = useState(null);
 
     // saving report
@@ -49,6 +56,8 @@ function PostTreatment() {
                         lastName: data.last_name,
                         phoneNumber: data.phone_num.toString(),
                         mrn: data.medical_ref_number.toString(),
+                        deviceUsed: data.medical_device_id,
+                        dateOfBirth: data.date_of_birth.toString()
                     });
                 } else {
                     message.error("Error retrieving patient information.");
@@ -61,15 +70,52 @@ function PostTreatment() {
                 if (treatmentResponse.status === 200) {
                     const data = treatmentResponse.data;
                     setTreatmentData({
+                        notes: data.notes,
                         drugVolume: data.drug_volume_administered,
                         solventVolume: data.wash_volume_administered,
                         laserPower1: data.power_delivered_by_laser_1,
                         laserPower2: data.power_delivered_by_laser_2,
                         laserPower3: data.power_delivered_by_laser_3,
                         laserPower4: data.power_delivered_by_laser_4,
+                        firstWait: data.first_wait.toString(),
+                        secondWait: data.second_wait.toString(),
+                        estimatedDurationForDrug: data.estimated_duration_for_drug_administration.toString(),
+                        estimatedDurationForLight: data.estimated_duration_for_light_administration.toString(),
+                        estimatedDurationForWash: data.estimated_duration_for_wash_administration.toString(),
+                        issues: data.issues,
+                        startTime: data.start_time.toString(),
+                        endTime: data.end_time.toString(),
+                        sessionNumber: data.session_number.toString(),
+                        painScore: data.pain_score.toString(),
+                        imageUrls: ["https://t3.ftcdn.net/jpg/07/84/19/86/360_F_784198690_4iOOWmDumjnUiP85gFJdZ5lXHzEq1ojS.jpg"]
                     });
                 } else {
                     message.error("Error retrieving treatment information.");
+                }
+                // Fetch clinician information
+                const clinicianResponse = await axios.get(
+                    `${getUsersAPIUrl()}/users/get_clinician_info?email=${clinicianEmail}`
+                );
+                if (clinicianResponse.status === 200) {
+                    const data = clinicianResponse.data.message;
+                    setClinicianData({
+                        firstName: data.first_name,
+                        lastName: data.last_name
+                    })
+                } else {
+                    message.error("Error retrieving clinician information.");
+                }
+                 // Fetch wound information
+                const woundResponse = await axios.get(
+                    `${getTreatmentAPIUrl()}/treatment/get_wound_info?id=${woundId}`
+                );
+                if (woundResponse.status === 200) {
+                    const data = woundResponse.data;
+                    setWoundData({
+                        treated: data.treated ? "Yes": "No"
+                    })
+                } else {
+                    message.error("Error retrieving wound information.");
                 }
             } catch (error) {
                 message.error("An error occurred while fetching data.");
@@ -77,11 +123,12 @@ function PostTreatment() {
         };
 
         fetchReportData();
-    }, [patientEmail, treatmentId]);
+    }, [woundId, treatmentId, patientEmail, clinicianEmail]);
 
     // setting file data if patient data and treatment data have been fetched
     useEffect(() => {
-        if (patientData && treatmentData) {
+        // only setting data if all of it has been successfully fetched
+        if (patientData && treatmentData && clinicianData && woundData) {
             setFileData({
                 name: {
                     fullName: `${patientData.firstName} ${patientData.lastName}`,
@@ -96,28 +143,67 @@ function PostTreatment() {
                             { name: "First Name", content: patientData.firstName, inlineContent: true },
                             { name: "Last Name", content: patientData.lastName, inlineContent: true },
                             { name: "Phone Number", content: patientData.phoneNumber, inlineContent: true },
+                            { name: "Email", content: patientEmail, inlineContent: true },
+                            { name: "Date of Birth", content: patientData.dateOfBirth, inlineContent: true },
+                            { name: "Medical Reference Number", content: patientData.mrn, inlineContent: true },
+                            { name: "Medical Device Identifier", content: patientData.deviceUsed, inlineContent: true }
                         ],
                     },
                     {
-                        title: "Treatment Dosages",
+                        title: "Clinician Identification",
                         fields: [
+                            { name: "First Name", content: clinicianData.firstName, inlineContent: true },
+                            { name: "Last Name", content: clinicianData.lastName, inlineContent: true },
+                            { name: "Email", content: clinicianEmail, inlineContent: true },
+                        ],
+                    },
+                    {
+                        title: "Wound Details",
+                        fields: [
+                            { name: "Treated", content: woundData.treated, inlineContent: true }
+                        ]
+                    },
+                    {
+                        title: "Treatment Details",
+                        fields: [
+                            { name: "Session Number", content: treatmentData.sessionNumber, inlineContent: true},
+                            { name: "Start Time", content: `${treatmentData.startTime}`, inlineContent: true },
+                            { name: "End Time", content: `${treatmentData.endTime}`, inlineContent: true },
+                            { name: "Patient's Pain Score", content: treatmentData.painScore, inlineContent: true },
                             { name: "Drug Volume", content: `${treatmentData.drugVolume} mL`, inlineContent: true },
                             { name: "Solvent Volume", content: `${treatmentData.solventVolume} mL`, inlineContent: true },
                             { name: "Laser 1's Power Level", content: `${treatmentData.laserPower1} mW`, inlineContent: true },
                             { name: "Laser 2's Power Level", content: `${treatmentData.laserPower2} mW`, inlineContent: true },
                             { name: "Laser 3's Power Level", content: `${treatmentData.laserPower3} mW`, inlineContent: true },
                             { name: "Laser 4's Power Level", content: `${treatmentData.laserPower4} mW`, inlineContent: true },
+                            { name: "Delay Between Drug And Light Administration", content: `${treatmentData.firstWait} s`, inlineContent: false },
+                            { name: "Delay Between Light and Solvent Administration", content: `${treatmentData.secondWait} s`, inlineContent: false },
+                            { name: "Estimated Duration for Drug Administration", content: `${treatmentData.estimatedDurationForDrug} s`, inlineContent: false },
+                            { name: "Estimated Duration for Light Administration", content: `${treatmentData.estimatedDurationForLight} s`, inlineContent: false },
+                            { name: "Images", type: "imageArray", content: treatmentData.imageUrls }
                         ],
+                    },
+                    {
+                        title: "Treatment Analysis",
+                        fields: [
+                            { name: "Clinician Notes", content: treatmentData.notes, inlineContent: false},
+                            { name: "Clinician-noted Issues", type: "textArray", content: treatmentData.issues }
+                        ]
                     },
                 ],
             });
         }
-    }, [patientData, treatmentData, treatmentId]);
+    }, [patientData, treatmentData, clinicianData, woundData]);
 
     return (
         <div className={styles.container}>
-            {fileData ? <ReportGeneration fileData={fileData} /> : <p>Loading report preperations...</p>}
-            <Button onClick={() => saveReport()}>Save Report</Button>
+            {fileData ?
+            <>
+                <ReportGeneration fileData={fileData} />
+                <Button icon={<SaveOutlined />} size="large" className={styles.saveButton} onClick={() => saveReport()}>Save Report</Button>
+            </>
+            : <p>Loading report preperations...</p>
+            }
         </div>
     );
 }
--- src/utilities/ReportGeneration/ReportGeneration.js
+++ src/utilities/ReportGeneration/ReportGeneration.js
@@ -1,54 +1,73 @@
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
-import { Button } from 'antd';
+import { Button, Dropdown } from 'antd';
+import { saveAs } from 'file-saver';
+import { DownloadOutlined } from '@ant-design/icons';
+import styles from '../../css/ReportGeneration.module.css'
+import { useState } from 'react';
 
-function ReportGeneration({fileData}) {
-    /*
+/*
 
-    How to pass fileData:
-    ----------------------------------------------------------------------------------------------------------------------
-    - Must contain a "name" key for information (as an object) required to name the file, which must contain:
-        - A "mrn" key for the patient's MRN (as a string)
-        - A "fullName" key for the patient's full name (as a string)
-        - A "treatmentId" key for the treatment ID for which this report is being generated (as a string)
-    - Must contain a "title" key for the title (as a string) of the report
-    - Must contain a "sections" key for an array of objects denoting each section, whereby each section must contain:
-        - A "title" key for the title (as a string) of that section
-        - A "fields" key for an array of objects denoting each field, whereby each field must contain:
-            - A "name" key for the name (as a string) of the field
-            - A "content" key for the actual content (as a string) for that respective field
-            - A "inlineContent" key that (as a a boolean) represents whether the content should be located to the right
-              of the field name (set to true) or if the content should start in the next line to provide extra space (set
-              to false)
+How to pass fileData:
+----------------------------------------------------------------------------------------------------------------------
+- Must contain a "name" key for information (as an object) required to name the file, which must contain:
+    - A "mrn" key for the patient's MRN (as a string)
+    - A "fullName" key for the patient's full name (as a string)
+    - A "treatmentId" key for the treatment ID for which this report is being generated (as a string)
+- Must contain a "title" key for the title (as a string) of the report
+- Must contain a "sections" key for an array of objects denoting each section, whereby each section must contain:
+    - A "title" key for the title (as a string) of that section
+    - A "fields" key for an array of objects denoting each field, whereby each field must contain:
+        - A "name" key for the name (as a string) of the field
+        - An optional "type" key for the type (as a string) of content. This can either be:
+            - "text" if the content is a string. If not provided, it will be defaulted to this.
+            - "imageArray" if the content is an array of image URLs as strings.
+            - "textArray" if the content is an array of strings.
+        - A "content" key for the actual content (as a string or array of image URLs) for that respective field
+        - A "inlineContent" key that (as a a boolean) represents whether the content should be located to the right
+            of the field name (set to true) or if the content should start in the next line to provide extra space (set
+            to false). This is only used if the "type" is set to text (or "type" is not provided).
 
-    Example:
-    {
-        name: {
-            mrn: "123",
-            fullName: "Jane Doe",
-            treatmentId: "1"
-        },
-        sections: [
-            {
-                title: "Treatment Details",
-                fields: [
-                    {
-                        name: "Date",
-                        content: "January 18, 2025"
-                        inlineContent: true
-                    }
-                ]
-            }
-        ]
-    }
-    ----------------------------------------------------------------------------------------------------------------------
-    */
+Example:
+{
+    name: {
+        mrn: "123",
+        fullName: "Jane Doe",
+        treatmentId: "1"
+    },
+    sections: [
+        {
+            title: "Treatment Details",
+            fields: [
+                {
+                    name: "Date",
+                    content: "January 18, 2025"
+                    inlineContent: true
+                },
+                {
+                    name: "Treatment Images",
+                    type: "imageArray"
+                    content: [
+                        "image1.com",
+                        "image2.com"
+                    ]
+                }
+            ]
+        }
+    ]
+}
+----------------------------------------------------------------------------------------------------------------------
+*/
+
+function ReportGeneration({fileData, disabled}) {
+    const [downloadInProgress, setDownloadInProgress] = useState(false);
 
     // file style properties (e.g. font, text size, bold, spacing)
     const fileStyle = {
         general: {
             startingX: 10,
-            indent: 60
+            startingY: 10,
+            indent: 70
         },
 
         title: {
@@ -77,22 +96,90 @@ function ReportGeneration({fileData}) {
             afterSectionTitle: 10,
             afterFieldRow: 7,
             afterExtraSpaceRow: 10,
-            afterSection: 10
+            afterSection: 10,
+            afterBulletList: 5
         },
 
         underline: {
             lineWidth: 0.5,
             margin: 2,
             length: 200
+        },
+
+        image: {
+            size: 50
+        },
+        
+        bullets: {
+            indent: 15
         }
     }
 
+    // naming convention: <patient-MRN>_<patient-name>_<treatment-id>
+    // name is all lowercase
+    const createTitle = () => {
+        return `${fileData.name.mrn.toLowerCase()}_${fileData.name.fullName.toLowerCase()}_${fileData.name.treatmentId}`;
+    }
+
+    // create, format, and download CSV
+    const downloadAsCSV = () => {
+        // disabling button
+        setDownloadInProgress(true);
+        // creating a string that will contain csv content
+        let csvContent = "";
+    
+        // adding title
+        const title = fileData.title;
+        csvContent += `${title}\n`;
+    
+        // creating and formating each section
+        fileData.sections.forEach((section, sectionIndex) => {
+            // adding section title
+            csvContent += `${section.title}\n`;
+    
+            // creating and formating each field
+            section.fields.forEach((field) => {
+                if (field.content === '' || field.content === null) { // skipping null content (error checking)
+                    return;
+                }
+    
+                // adding field and content in side-by-side cells
+                csvContent += `${field.name},"${field.content}"\n`;
+            });
+    
+            // adding spacing after section (except on the last iteration)
+            if (sectionIndex < fileData.sections.length - 1) {
+                csvContent += "\n";
+            }
+        });
+    
+        // downloading as CSV file
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
+        saveAs(blob, `${createTitle()}.csv`);
+        // enabling button
+        setDownloadInProgress(false)
+    };    
+
     // create, format, and download PDF
-    const downloadAsPDF = () => {
+    const downloadAsPDF = async () => {
+        // disabling button
+        setDownloadInProgress(true)
         const pdf = new jsPDF();
+        const pageHeight = pdf.internal.pageSize.height; // getting page height
+
+        // helper function to check if a new page is needed
+        const checkPageBreak = (currentY, spaceNeeded) => {
+            if (currentY + spaceNeeded > pageHeight - fileStyle.general.startingY) {
+                pdf.addPage(); // Add a new page
+                return fileStyle.general.startingY;
+            }
+            return currentY;
+        };
 
         // helper function to draw section titles
         const drawSectionTitle = (pdf, title, y) => {
+            // checking if new page needed
+            y = checkPageBreak(y, fileStyle.spacing.afterSectionTitle)
             // setting text properties
             pdf.setFontSize(fileStyle.sectionTitle.size);
             pdf.setFont(fileStyle.sectionTitle.font, fileStyle.sectionTitle.type);
@@ -104,6 +191,8 @@ function ReportGeneration({fileData}) {
 
         // helper function to draw field and content inline
         const drawFieldContentInline = (pdf, field, content, y) => {
+            // checking if new page needed
+            y = checkPageBreak(y, fileStyle.spacing.afterFieldRow)
             // setting text properties
             pdf.setFontSize(fileStyle.fields.size);
             pdf.setFont(fileStyle.fields.font, fileStyle.fields.type.name);
@@ -113,53 +202,131 @@ function ReportGeneration({fileData}) {
             return y + fileStyle.spacing.afterFieldRow;
         };
 
-        // Helper function to draw field with content on a new line
+        // helper function to draw field with content on a new line
         const drawFieldContentExtraSpace = (pdf, field, content, y) => {
+            // checking if new page needed
+            y = checkPageBreak(y, fileStyle.spacing.afterExtraSpaceRow + fileStyle.spacing.afterFieldRow)
             // setting text properties
             pdf.setFontSize(fileStyle.fields.size);
             pdf.setFont(fileStyle.fields.font, fileStyle.fields.type.name);
             pdf.text(field, fileStyle.general.startingX, y);
             pdf.setFont(fileStyle.fields.font, fileStyle.fields.type.content);
-            y += fileStyle.spacing.afterFieldRow; // Move to the next line for the content
+            y += fileStyle.spacing.afterFieldRow; // move to the next line for the content
             pdf.text(content, fileStyle.general.startingX, y);
-            return y + fileStyle.spacing.afterExtraSpaceRow; // Add spacing after the content
+            return y + fileStyle.spacing.afterExtraSpaceRow;
+        };
+
+        // helper function to show images
+        const drawFieldContentWithImages = async (pdf, field, images, y) => {
+            // checking if new page needed
+            y = checkPageBreak(y, fileStyle.spacing.afterFieldRow)
+            // setting text properties
+            pdf.setFontSize(fileStyle.fields.size);
+            pdf.setFont(fileStyle.fields.font, fileStyle.fields.type.name);
+            pdf.text(field, fileStyle.general.startingX, y);
+            y += fileStyle.spacing.afterFieldRow
+            for (let i = 0; i < images.length; i++) {
+                // checking if new page needed
+                y = checkPageBreak(y, 2*fileStyle.image.size + fileStyle.spacing.afterFieldRow)
+                const img = images[i];
+                const imgData = await fetch(img)
+                    .then(res => res.blob())
+                    .then(blob => new Promise((resolve, reject) => {
+                        const reader = new FileReader();
+                        reader.onloadend = () => resolve(reader.result);
+                        reader.onerror = reject;
+                        reader.readAsDataURL(blob);
+                    }));
+    
+                pdf.addImage(imgData, 'JPEG', fileStyle.general.startingX, y, fileStyle.image.size, fileStyle.image.size);
+                y += fileStyle.image.size + fileStyle.spacing.afterFieldRow;
+            }
+            return y + fileStyle.spacing.afterFieldRow;
+        };
+
+        // helper function to draw a bullet list
+        const drawBulletList = (pdf, field, items, y) => {
+            // checking if new page is needed
+            y = checkPageBreak(y, fileStyle.spacing.afterSectionTitle)
+            // setting text properties
+            pdf.setFontSize(fileStyle.fields.size);
+            pdf.setFont(fileStyle.fields.font, fileStyle.fields.type.name);
+            pdf.text(field, fileStyle.general.startingX, y);
+            y += fileStyle.spacing.afterFieldRow // new line for bullets
+            pdf.setFontSize(fileStyle.fields.size);
+            pdf.setFont(fileStyle.fields.font, fileStyle.fields.type.content);
+            items.forEach((item) => {
+                // adding each array item as a bullet
+                pdf.text(`• ${item}`, fileStyle.bullets.indent, y);
+                y += fileStyle.spacing.afterFieldRow;
+            });
+            return y + fileStyle.spacing.afterBulletList;
         };
 
         // adding report title
         const title = fileData.title;
         pdf.setFontSize(fileStyle.title.size);
         pdf.setFont(fileStyle.title.font, fileStyle.title.type);
-        pdf.text(title, fileStyle.general.startingX, fileStyle.general.startingX);
+        pdf.text(title, fileStyle.general.startingX, fileStyle.general.startingY);
 
         // start position for the first section
         let y = fileStyle.spacing.afterTitle;
 
         // creating and formating each section
-        fileData.sections.forEach((section, index) => {
+        for (const section of fileData.sections) {
             y = drawSectionTitle(pdf, section.title, y) // adding title
             // creating and formatting each field
-            section.fields.forEach((field) => {
-                if(field.inlineContent) { // adding inline content
+            for (const field of section.fields) {
+                if(field.content == '' || field.content == null) { // skipping null content (error checking)
+                    continue
+                }
+                if(field.type == "imageArray") { // adding images
+                    y = await drawFieldContentWithImages(pdf, `${field.name}:`, field.content, y)
+                } else if(field.type == "textArray") { // adding list
+                    y = drawBulletList(pdf, `${field.name}:`, field.content, y)
+                } else if(field.inlineContent) { // adding inline content (assuming type is text)
                     y = drawFieldContentInline(pdf, `${field.name}:`, field.content, y)
-                } else { // adding extra space
+                } else { // adding extra space (assuming type is text)
                     y = drawFieldContentExtraSpace(pdf, `${field.name}:`, field.content, y)
                 }
-            })
+            }
+            
             // adding spacing after section (except on last iteration)
-            if (index < fileData.sections.length - 1) {
+            if (section !== fileData.sections[fileData.sections.length - 1]) {
                 y += fileStyle.spacing.afterSection;
             }
-        })
+        }
 
         // downloading PDF
-        // naming convention: <patient-MRN>_<patient-name>_<treatment-id>.pdf
-        // name is all lowercase
-        pdf.save(`${fileData.name.mrn.toLowerCase()}_${fileData.name.fullName.toLowerCase()}_${fileData.name.treatmentId}`);
+        pdf.save(`${createTitle()}`);
+        // enabling button
+        setDownloadInProgress(false)
     };
 
-    return <div>
-        <Button onClick={downloadAsPDF}>Download as PDF</Button>
-    </div>
+    const menu = {
+        items: [
+            {
+                key: '1',
+                label: 'Download as PDF',
+                onClick: () => downloadAsPDF(),
+            },
+            {
+                key: '2',
+                label: 'Download as CSV',
+                onClick: () => downloadAsCSV(),
+            },
+        ],
+    };
+
+    return (
+        <div>
+            <Dropdown disabled={disabled && downloadInProgress}menu={menu} trigger={['hover']}>
+                <Button className={styles.downloadButton} icon={<DownloadOutlined />} size="large">
+                    Download Report
+                </Button>
+            </Dropdown>
+        </div>
+    );
 }
 
 export default ReportGeneration;
