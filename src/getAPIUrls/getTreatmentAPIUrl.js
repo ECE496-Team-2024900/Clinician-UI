e24e08bd8c24b8d0050ef98d6f335ff24ac3fb8e
zoya5636 <zoya@chishtie.com>
2025-03-26 21:41:35 -0400

    changed http to https

--- src/getAPIUrls/getTreatmentAPIUrl.js
+++ src/getAPIUrls/getTreatmentAPIUrl.js
@@ -2,7 +2,7 @@ import { clientHost } from "./clientHost.js";
 
 const hostOptions = {
     local: "http://127.0.0.1:8000",
-    production: "http://treatment-t0m8.onrender.com"
+    production: "https://treatment-t0m8.onrender.com"
 } 
 export function getTreatmentAPIUrl() {
     if(clientHost !== "production") {
